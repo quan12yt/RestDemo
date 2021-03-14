6b3606b76636387135fdbeebb180472f229169cf
len <58143415+LenHoChi@users.noreply.github.com>
2021-03-14 18:49:54 +0700

    [Project]

--- src/main/java/com/example/demo/services/ProjectService.java
+++ src/main/java/com/example/demo/services/ProjectService.java
@@ -1,9 +1,6 @@
 package com.example.demo.services;
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.example.demo.model.Project;
 
 public interface ProjectService {
